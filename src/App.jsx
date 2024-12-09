16c609fd681eab3e807433a222c406b7cde51fe7
micko2001 <micko.wijayanto57@gmail.com>
2024-12-09 13:23:47 +0700

    implement navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
+import logo from "./logo/wallet.png";
+
+import NavItems from "./components/NavItems";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#",
+    },
+    {
+      title: "Transfer",
+      link: "#",
+    },
+    {
+      title: "Topup",
+      link: "#",
+    },
+    {
+      title: "Signout",
+      link: "#",
+    },
+  ];
   return (
     <>
-      <p className="text-9xl font-mono hover:font-serif">Hello World</p>
+      <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} />
+        <div className="flex gap-x-8">
+          <NavItems menu={menu} />
+        </div>
+      </nav>
     </>
   );
 }
